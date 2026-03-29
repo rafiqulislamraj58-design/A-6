ceb7f19050939c239677165f41bf407c954465e1
rafiqulislamraj58-design <rafiqulislamraj58@gmail.com>
2026-03-29 22:41:09 +0600

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+import Nav from './Componatnt/Nav'
 
 const App = () => {
   return (
     <>
-
+   <Nav></Nav>
     </>
   )
 }
---
+++ src/Componatnt/Nav.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='flex justify-between p-6 bg-zinc-50 shadow-lg'>
+    <h2 className=' font-bold text-2xl bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent'>DigiTools</h2>
+    
+    <div className='flex  gap-4 left-2.5'>
+        <p className='text-[#101727] font-bold hover:text-pink-500  cursor-pointer'>Products</p>
+        <p className='text-[#101727] font-bold hover:text-pink-500  cursor-pointer'>Features</p>
+        <p className='text-[#101727] font-bold hover:text-pink-500  cursor-pointer'>Pricing</p>
+        <p className='text-[#101727] font-bold hover:text-pink-500  cursor-pointer'>Testimonials</p>
+        <p className='text-[#101727] font-bold hover:text-pink-500  cursor-pointer'>FAQ</p>
+        
+    </div>
+    <div className='flex gap-3.5 items-center'>
+    <p className='bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent'><i className="ri-shopping-cart-2-line text-2xl"></i></p>
+    <p className='  font-bold text-[#101727]'>Login</p>
+    <button className='bg-gradient-to-r from-purple-500 to-pink-500 px-3.5 py-1.5 rounded-full text-white font-bold'>Get Started</button>
+    </div>
+    
+    </div>
+  )
+}
+
+export default Nav
