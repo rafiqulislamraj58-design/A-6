2034714ef15050a93f1f6b2e3e63f0d3e713629e
rafiqulislamraj58-design <rafiqulislamraj58@gmail.com>
2026-04-01 13:40:40 +0600

    dasi ui broken not worked work with cnd and talwind

--- src/Componatnt/Priceing.jsx
+++ src/Componatnt/Priceing.jsx
@@ -4,124 +4,124 @@ const Priceing = () => {
   return (
    <div className='container mx-auto'>
 
-  <div className='container mx-auto rounded-md border-zinc-400 border-2 mt-3.5 mb-3.5 overflow-hidden p-10 '>
+  <div className='container mx-auto rounded-md border-zinc-400 border-2 mt-3.5 mb-3.5 p-10 '>
     <div className='text-center mt-2.5 mb-3.5'>
     <h1 className='font-bold text-2xl '>Simple, Transparent Pricing</h1>
     <p className='text-gray-400'>Choose the plan that fits your needs. Upgrade or downgrade anytime.</p>
     </div>
-    <div className=' sm:grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4.5'>
- <div class="card w-full bg-base-100 shadow-sm p-10 hover:translate-y-0.5 hover:shadow-lg shadow-purple-500/50">
-  <div class="card-body">
-    <span class="badge badge-xs badge-warning">Starter</span>
-    <div class="flex justify-between">
-      <h2 class="text-xl ">Perfect for getting started</h2>
+    <div className=' sm:grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-5'>
+ <div className="card w-full bg-base-100 shadow-sm p-10 hover:translate-y-0.5 hover:shadow-lg shadow-purple-500/50">
+  <div className="card-body">
+    <span className="badge badge-xs badge-warning">Starter</span>
+    <div className="flex justify-between">
+      <h2 className="text-xl ">Perfect for getting started</h2>
       
     </div>
-    <h2 class="text-xl font-bold">$0/month</h2>
-    <ul class="mt-6 flex flex-col gap-2 text-xs">
+    <h2 className="text-xl font-bold">$0/month</h2>
+    <ul className="mt-6 flex flex-col gap-2 text-xs">
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Access to 10 free tools</span>
       </li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Basic templates</span>
       </li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Community support</span>
       </li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>1 project per month</span>
       </li>
      
     </ul>
-    <div class="mt-6">
-      <button class="btn bg-gradient-to-r shadow-none w-full from-purple-500 to-pink-500 rounded-full text-white ">Get Started Free</button>
+    <div className="mt-6">
+      <button className="btn bg-gradient-to-r shadow-none w-full from-purple-500 to-pink-500 rounded-full text-white ">Get Started Free</button>
     </div>
   </div>
 </div>
- <div class="card w-full bg-base-100 p-10 border-2 border-purple-500 scale-105 shadow-lg">
-  <div class="card-body">
-    <span class="badge badge-xs badge-warning">Most Popula</span>
-    <div class="flex justify-between">
-      <h2 class="text-xl ">Perfect for getting started</h2>
+ <div className="card w-full bg-base-100 p-10 border-2 border-purple-500 scale-105 shadow-lg">
+  <div className="card-body">
+    <span className="badge badge-xs badge-warning">Most Popula</span>
+    <div className="flex justify-between">
+      <h2 className="text-xl ">Perfect for getting started</h2>
      
     </div>
-     <h2 class="text-xl font-bold">$29/month</h2>
-    <ul class="mt-6 flex flex-col gap-2 text-xs">
+     <h2 className="text-xl font-bold">$29/month</h2>
+    <ul className="mt-6 flex flex-col gap-2 text-xs">
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Access to all premium tools</span>
       </li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Unlimited templates</span>
       </li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Priority support</span>
       </li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Unlimited projects</span>
       </li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Cloud sync</span>
       </li>
      
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Advanced analytics</span>
       </li>
      
     </ul>
-    <div class="mt-6">
-      <button class="btn bg-gradient-to-r shadow-none w-full from-purple-500 to-pink-500 rounded-full text-white ">Get Start Pro Trial</button>
+    <div className="mt-6">
+      <button className="btn bg-gradient-to-r shadow-none w-full from-purple-500 to-pink-500 rounded-full text-white ">Get Start Pro Trial</button>
     </div>
   </div>
 </div>
 
-<div class="card w-full bg-base-100 shadow-sm p-10 hover:translate-y-0.5 hover:shadow-lg shadow-purple-500/50">
-  <div class="card-body">
-    <span class="badge badge-xs badge-warning">Most Popula</span>
-    <div class="flex justify-between">
-      <h2 class="text-xl ">Enterprise</h2>
+<div className="card w-full bg-base-100 shadow-sm p-10 hover:translate-y-0.5 hover:shadow-lg shadow-purple-500/50">
+  <div className="card-body">
+    <span className="badge badge-xs badge-warning">Most Popula</span>
+    <div className="flex justify-between">
+      <h2 className="text-xl ">Enterprise</h2>
      
     </div>
-     <h2 class="text-xl font-bold">$99/month</h2>
-    <ul class="mt-6 flex flex-col gap-2 text-xs">
+     <h2 className="text-xl font-bold">$99/month</h2>
+    <ul className="mt-6 flex flex-col gap-2 text-xs">
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Everything in Pro</span>
       </li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Team collaboration</span>
       </li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Custom integrations</span>
       </li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Dedicated support</span>
       </li>
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>SLA guarantee</span>
       </li>
      
       <li>
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg>
         <span>Custom branding</span>
       </li>
      
     </ul>
-    <div class="mt-6">
-      <button class="btn bg-gradient-to-r shadow-none w-full from-purple-500 to-pink-500 rounded-full text-white ">Get Contact Sales</button>
+    <div className="mt-6">
+      <button className="btn bg-gradient-to-r shadow-none w-full from-purple-500 to-pink-500 rounded-full text-white ">Get Contact Sales</button>
     </div>
   </div>
 </div>
--- src/Componatnt/Start.jsx
+++ src/Componatnt/Start.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
-
-const Start = ({active ,setactive}) => {
+const Start = ({ active, setactive }) => {
   return (
-    <div>
-        <div className="tabs tabs-box bg-transparent justify-center">
-  <input type="radio" name="my_tabs_1" className="tab rounded-full w-40 " aria-label="Products"  onClick={()=>setactive("Products") } defaultChecked/>
-  <input type="radio" name="my_tabs_1" className="tab rounded-full w-40 " aria-label="Cart" 
-  onClick={()=>setactive("Cart")}  />
-</div>
+    <div className="flex justify-center gap-4 mt-5">
+      <button
+        onClick={() => setactive("Products")}
+        className={`px-6 py-2 rounded-full transition ${
+          active === "Products"
+            ? "bg-purple-500 text-white"
+            : "bg-gray-200 text-black"
+        }`}
+      >
+        Products
+      </button>
+      <button
+        onClick={() => setactive("Cart")}
+        className={`px-6 py-2 rounded-full transition ${
+          active === "Cart"
+            ? "bg-purple-500 text-white"
+            : "bg-gray-200 text-black"
+        }`}
+      >
+        Cart
+      </button>
     </div>
   )
 }
